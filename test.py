06bf3d49715da76cb760fedb0221a5710d94cc50
Ethan Brown <ewb@ewb.io>
2017-09-20 20:34:57 +0200

    simplified

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 from drawille import Canvas
-from math import sqrt, atan, cos, sin, pi
-
-c = Canvas()
+from math import sqrt
 
 class Node(object):
     """Class to draw a box
@@ -22,15 +20,6 @@ class Node(object):
         self.height = height
         self.label = label
 
-    def draw(self):
-        for i in range(self.width):
-            c.set(self.x0 + i, self.y0)
-            c.set(self.x0 + i, self.y0 + self.height)
-        for i in range(self.height):
-            c.set(self.x0, self.y0 + i)
-            c.set(self.x0 + self.width, self.y0 + i)
-        c.set_text(self.x0 + (self.width // 2), self.y0 + (self.height // 2), self.label)
-
 class Edge(object):
     """Class to draw an edge between Nodes
 
@@ -45,45 +34,73 @@ class Edge(object):
         self.n1 = n1
         self.label = label
 
-    def draw(self):
-        #  (n0)
-        #     .
-        #      .
-        #      (n1)
-        x0 = self.n0.x0 + (self.n0.width // 2)
-        x1 = self.n1.x0 + (self.n1.width // 2)
-        y0 = self.n0.y0 + (self.n0.height // 2)
-        y1 = self.n1.y0 + (self.n1.height // 2)
-        x_sign = 1 if x1 > x0 else -1
-        y_sign = 1 if y1 > y0 else -1
-        width = abs(x1-x0)
-        height = abs(y1-y0)
-        l = sqrt(width**2 + height**2)
-        theta = 0.5*pi if not height else atan(float(width) / float(height))
+class Graph(object):
+    """Graph class
+
+       Attributes:
+           nodes (list(Node)): List of nodes
+           edges (list(Edge)): List of edges
+           c (drawille.Canvas): Canvas object
+    """
+
+    def __init__(self):
+        self.nodes = {}
+        self.edges = []
+        self.c = Canvas()
+
+    def add_node(self, label):
+        self.nodes[label] = Node((len(self.nodes)*20) % 40, len(self.nodes)*20, 10, 10, label)
+
+    def add_edge(self, label0, label1, label=""):
+        self.edges.append(Edge(self.nodes[label0], self.nodes[label1], label))
+
+    def draw_node(self, n):
+        for i in range(n.width):
+            self.c.set(n.x0 + i, n.y0)
+            self.c.set(n.x0 + i, n.y0 + n.height)
+        for i in range(n.height):
+            self.c.set(n.x0, n.y0 + i)
+            self.c.set(n.x0 + n.width, n.y0 + i)
+        self.c.set_text(n.x0 + (n.width // 2), n.y0 + (n.height // 2), n.label)
+
+    def draw_edge(self, e):
+        x0 = e.n0.x0 + (e.n0.width // 2)
+        x1 = e.n1.x0 + (e.n1.width // 2)
+        y0 = e.n0.y0 + (e.n0.height // 2)
+        y1 = e.n1.y0 + (e.n1.height // 2)
+        x_diff = x1-x0
+        y_diff = y1-y0
+        l = sqrt(x_diff**2 + y_diff**2)
+        m = (y_diff / x_diff) if x_diff else 0
+        dx = x_diff / l if l else 0
+        dy = y_diff / l if l else 0
         for i in range(int(l)):
-            x = x0 + i*sin(theta)*x_sign
-            y = y0 + i*cos(theta)*y_sign
+            x = x0 + i*dx
+            y = y0 + i*dy
             if (
-                abs(x-x0) > (self.n0.width // 2) and
-                abs(x-x1) > (self.n1.width // 2)
+                abs(x-x0) > (e.n0.width // 2) and
+                abs(x-x1) > (e.n1.width // 2)
             ) or (
-                abs(y-y0) > (self.n0.height // 2) and
-                abs(y-y1) > (self.n1.height // 2)
-            ): c.set(x, y)
-            if i == l // 2:
-                c.set_text(x, y, self.label)
+                abs(y-y0) > (e.n0.height // 2) and
+                abs(y-y1) > (e.n1.height // 2)
+            ): self.c.set(x, y)
+        self.c.set_text(x0 + (l // 2)*dx, y0 + (l // 2)*dy, e.label)
 
+    def draw(self):
+        # TODO: draw to minimize edge crossings
+        for label in self.nodes:
+            self.draw_node(self.nodes[label])
+        for edge in self.edges:
+            self.draw_edge(edge)
+        print(self.c.frame())
 
-n0 = Node(0, 20, 10, 10, 'n0')
-n0.draw()
-n1 = Node(20, 20, 10, 10, 'n1')
-n1.draw()
-n2 = Node(25, 0, 10, 10, 'n2')
-n2.draw()
-e = Edge(n0, n2, 'e0')
-e.draw()
-e = Edge(n2, n1, 'e1')
-e.draw()
-e = Edge(n1, n0, 'e2')
-e.draw()
-print(c.frame())
+g = Graph()
+g.add_node('n0')
+g.add_node('n1')
+g.add_node('n2')
+g.add_node('n3')
+g.add_node('n4')
+g.add_edge('n0', 'n1', 'e0')
+g.add_edge('n0', 'n2', 'e1')
+g.add_edge('n2', 'n1', 'e2')
+g.draw()
