c2c9d76bfe05bffff7589af27d7f81a3abe8b845
Ethan Brown <ewb@ewb.io>
2017-09-21 10:53:37 +0200

    First version complete

---
+++ graphscii/__init__.py
@@ -0,0 +1,190 @@
+from __future__ import print_function
+from math import sqrt
+from drawille import Canvas
+
+class Node(object):
+    """Node class
+
+       Attributes:
+           label (str): Label for the node
+           att (dict(str, val)): Dictionary of attributes for the node
+           pos (list(float)): x, y position between 0 and 1
+           shape (list(float)): Shape of node
+           show_label (bool): Whether or not to show the label
+           show_att (bool): Whether or not to show the attributes
+    """
+
+    def __init__(self, label, att={}, pos=[0, 0], shape=[10, 10], show_label=True, show_att=False):
+        """Initialize Node class
+
+           Args:
+               label (str): Label for the node
+               att (dict(str, val)): Dictionary of attributes for the node
+               pos (list(float)): x, y position between 0 and 1
+               shape (list(float)): Shape of node
+               show_label (bool): Whether or not to show the label
+               show_att (bool): Whether or not to show the attributes
+        """
+        if len(pos) != 2: raise ValueError('pos must be of length 2')
+        if pos[0] < 0 or pos[0] > 1: raise ValueError('x position must be between 0 and 1')
+        if pos[1] < 0 or pos[1] > 1: raise ValueError('y position must be between 0 and 1')
+        if len(shape) != 2: raise ValueError('shape must be of length 2')
+        self.label = label
+        self.att = att
+        self.pos = pos
+        self.shape = shape
+        self.show_label = show_label
+        self.show_att = show_att
+
+class Edge(object):
+    """Edge class
+
+       Attributes:
+           n0 (Node): Starting node
+           n1 (Node): Ending node
+           label (str): Label
+           att (dict(str, val)): Dictionary of attributes
+           show_label (bool): Whether or not to show the label
+           show_att (bool): Whether or not to show the attributes
+    """
+
+    def __init__(self, n0, n1, label, att, show_label=True, show_att=False):
+        """Initialize Edge class
+
+           Args:
+               n0 (Node): Starting node
+               n1 (Node): Ending node
+               label (str): Label
+               att (dict(str, val)): Dictionary of attributes
+               show_label (bool): Whether or not to show the label
+               show_att (bool): Whether or not to show the attributes
+        """
+        self.n0 = n0
+        self.n1 = n1
+        self.label = label
+        self.att = att
+        self.show_label = show_label
+        self.show_att = show_att
+
+class Graph(object):
+    """Graph class
+
+       Attributes:
+           max_x (int): Maximum horizontal canvas size
+           max_y (int): Maximum vertical canvas size
+           default_shape (list(float)): Default node shape
+           nodes (list(Node)): List of nodes
+           edges (list(Edge)): List of edges
+           c (drawille.Canvas): Canvas object
+    """
+
+    def __init__(self, max_x=300, max_y=135, default_shape=[10, 10]):
+        """Initialize Graph
+
+           Args:
+               max_x (int): Maximum horizontal canvas size
+               max_y (int): Maximum vertical canvas size
+               default_shape (list(float)): Default node shape
+        """
+        self.max_x = max_x
+        self.max_y = max_y
+        self.default_shape = default_shape
+        self.nodes = {}
+        self.edges = []
+        self.c = Canvas()
+
+    def add_node(self, label, att={}, pos=[0, 0], shape=None):
+        """Add a node
+
+           Args:
+               label (str): Label for the node
+               att (dict(str, val)): Dictionary of attributes for the node
+               pos (list(float)): x, y position between 0 and 1
+               shape (list(float)): Shape of node
+        """
+        if not shape:
+            shape = self.default_shape
+        self.nodes[label] = Node(label, att, pos, shape)
+
+    def add_edge(self, label0, label1, label='', att={}):
+        """Add an edge
+
+           Args:
+               label0 (str): Label for the starting node
+               label1 (str): Label for the ending node
+               label (str): Label for the edge
+               att (dict(str, val)): Dictionary of attributes for the edge
+        """
+        if not (label0 in self.nodes): raise ValueError(label0+' does not exist.')
+        if not (label1 in self.nodes): raise ValueError(label1+' does not exist.')
+        self.edges.append(Edge(self.nodes[label0], self.nodes[label1], label, att))
+
+    def draw_node(self, n):
+        """Add a node to the canvas
+
+           Args:
+               n (Node): Node to draw
+        """
+        x = float(n.pos[0] * self.max_x)
+        y = float(n.pos[1] * self.max_y)
+        half_width = n.shape[0] // 2
+        half_height = n.shape[1] // 2
+        for i in range(n.shape[0]):
+            self.c.set((x - half_width) + i, y - half_height)
+            self.c.set((x - half_width) + i, y + half_height)
+        for i in range(n.shape[1]):
+            self.c.set(x - half_width, (y - half_height) + i)
+            self.c.set(x + half_width, (y - half_height) + i)
+        label = ''
+        if n.show_label:
+            label += n.label
+        if n.show_att:
+            for key in n.att:
+                label += ', %s: %s'%(key, str(n.att[key]))
+        self.c.set_text(x - half_width + 3, y, label)
+
+    def draw_edge(self, e):
+        """Add an edge to the canvas
+
+           Args:
+               e (Edge): Edge to draw
+        """
+        x0 = float(e.n0.pos[0] * self.max_x)
+        y0 = float(e.n0.pos[1] * self.max_y)
+        x1 = float(e.n1.pos[0] * self.max_x)
+        y1 = float(e.n1.pos[1] * self.max_y)
+        half_width0 = e.n0.shape[0] // 2
+        half_width1 = e.n1.shape[0] // 2
+        half_height0 = e.n0.shape[1] // 2
+        half_height1 = e.n1.shape[1] // 2
+        x_diff = x1 - x0
+        y_diff = y1 - y0
+        l = sqrt(x_diff**2 + y_diff**2)
+        dx = x_diff / l if l else 0
+        dy = y_diff / l if l else 0
+        for i in range(int(l)):
+            x = x0 + i*dx
+            y = y0 + i*dy
+            if (
+                abs(x - x0) > half_width0 and
+                abs(x - x1) > half_width1
+            ) or (
+                abs(y - y0) > half_height0 and
+                abs(y - y1) > half_height1
+            ): self.c.set(x, y)
+        label = ''
+        if e.show_label:
+            label += e.label
+        if e.show_att:
+            for key in n.att:
+                label += ', %s: %s'%(key, str(n.att[key]))
+        self.c.set_text(x0 + (l // 2)*dx, y0 + (l // 2)*dy, label)
+
+    def draw(self):
+        """Draw the graph
+        """
+        for node in self.nodes.itervalues():
+            self.draw_node(node)
+        for edge in self.edges:
+            self.draw_edge(edge)
+        print(self.c.frame())
--- risk.py
+++ risk.py
@@ -1,256 +1,143 @@
-from __future__ import print_function
-from math import sqrt
-from drawille import Canvas
+from graphscii import Graph
 
-class Node(object):
-    """Node class
+g = Graph()
 
-       Attributes:
-           x (int): x-position
-           y (int): y-position
-           label (str): Label
-           att (dict(str, val)): Dictionary of attributes
-    """
+# North America
+g.add_node('Alaska', pos=[0, 0])
+g.add_node('Northwest Territory', pos=[0.1, 0.1])
+g.add_node('Greenland', pos=[0.3, 0.05])
+g.add_node('Alberta', pos=[0.05, 0.2])
+g.add_node('Ontario', pos=[0.15, 0.3])
+g.add_node('Quebec', pos=[0.25, 0.25])
+g.add_node('Western US', pos=[0.05, 0.4])
+g.add_node('Eastern US', pos=[0.25, 0.45])
+g.add_node('Central America', pos=[0.15, 0.6])
+g.add_edge('Alaska', 'Northwest Territory')
+g.add_edge('Alaska', 'Alberta')
+g.add_edge('Northwest Territory', 'Alberta')
+g.add_edge('Northwest Territory', 'Ontario')
+g.add_edge('Northwest Territory', 'Greenland')
+g.add_edge('Alberta', 'Ontario')
+g.add_edge('Alberta', 'Western US')
+g.add_edge('Ontario', 'Greenland')
+g.add_edge('Ontario', 'Quebec')
+g.add_edge('Ontario', 'Western US')
+g.add_edge('Ontario', 'Eastern US')
+g.add_edge('Greenland', 'Quebec')
+g.add_edge('Western US', 'Eastern US')
+g.add_edge('Western US', 'Central America')
+g.add_edge('Quebec', 'Eastern US')
+g.add_edge('Eastern US', 'Central America')
 
-    def __init__(self, x, y, label=""):
-        self.x = x
-        self.y = y
-        self.label = label
-        self.att = {}
+# South America
+g.add_node('Venezuela', pos=[0.2, 0.7])
+g.add_node('Brazil', pos=[0.3, 0.85])
+g.add_node('Peru', pos=[0.15, 0.8])
+g.add_node('Argentina', pos=[0.2, 1.0])
+g.add_edge('Venezuela', 'Brazil')
+g.add_edge('Venezuela', 'Peru')
+g.add_edge('Brazil', 'Peru')
+g.add_edge('Brazil', 'Argentina')
+g.add_edge('Peru', 'Argentina')
 
-class Edge(object):
-    """Edge class
+# Africa
+g.add_node('North Africa', pos=[0.45, 0.65])
+g.add_node('Egypt', pos=[0.55, 0.6])
+g.add_node('East Africa', pos=[0.65, 0.7])
+g.add_node('Congo', pos=[0.5, 0.8])
+g.add_node('South Africa', pos=[0.55, 1.0])
+g.add_node('Madagascar', pos=[0.6, 0.9])
+g.add_edge('North Africa', 'Egypt')
+g.add_edge('North Africa', 'East Africa')
+g.add_edge('North Africa', 'Congo')
+g.add_edge('Egypt', 'East Africa')
+g.add_edge('East Africa', 'Congo')
+g.add_edge('East Africa', 'Madagascar')
+g.add_edge('Congo', 'South Africa')
+g.add_edge('Madagascar', 'South Africa')
 
-       Attributes:
-           n0 (Node): Starting node
-           n1 (Node): Ending node
-           label (str): Label
-    """
+# Europe
+g.add_node('Western Europe', pos=[0.45, 0.5])
+g.add_node('Southern Europe', pos=[0.6, 0.45])
+g.add_node('Northern Europe', pos=[0.55, 0.35])
+g.add_node('Great Britain', pos=[0.4, 0.3])
+g.add_node('Iceland', pos=[0.4, 0.2])
+g.add_node('Scandinavia', pos=[0.55, 0.1])
+g.add_node('Ukraine', pos=[0.65, 0.2])
+g.add_edge('Western Europe', 'Southern Europe')
+g.add_edge('Western Europe', 'Northern Europe')
+g.add_edge('Western Europe', 'Great Britain')
+g.add_edge('Southern Europe', 'Northern Europe')
+g.add_edge('Southern Europe', 'Ukraine')
+g.add_edge('Northern Europe', 'Great Britain')
+g.add_edge('Northern Europe', 'Ukraine')
+g.add_edge('Northern Europe', 'Scandinavia')
+g.add_edge('Great Britain', 'Scandinavia')
+g.add_edge('Great Britain', 'Iceland')
+g.add_edge('Ukraine', 'Scandinavia')
+g.add_edge('Scandinavia', 'Iceland')
 
-    def __init__(self, n0, n1, label=""):
-        self.n0 = n0
-        self.n1 = n1
-        self.label = label
+# Asia
+g.add_node('Middle East', pos=[0.7, 0.55])
+g.add_node('Afghanistan', pos=[0.75, 0.4])
+g.add_node('India', pos=[0.8, 0.6])
+g.add_node('Ural', pos=[0.75, 0.15])
+g.add_node('China', pos=[0.85, 0.5])
+g.add_node('Siberia', pos=[0.825, 0.2])
+g.add_node('Mongolia', pos=[0.95, 0.4])
+g.add_node('Yakutsk', pos=[0.9, 0.1])
+g.add_node('Irkutsk', pos=[0.9, 0.25])
+g.add_node('Kamchatka', pos=[1.0, 0.0])
+g.add_node('Japan', pos=[1.0, 0.25])
+g.add_node('Siam', pos=[0.9, 0.65])
+g.add_edge('Middle East', 'Afghanistan')
+g.add_edge('Middle East', 'India')
+g.add_edge('Afghanistan', 'India')
+g.add_edge('Afghanistan', 'Ural')
+g.add_edge('Afghanistan', 'China')
+g.add_edge('India', 'China')
+g.add_edge('India', 'Siam')
+g.add_edge('Ural', 'China')
+g.add_edge('Ural', 'Siberia')
+g.add_edge('China', 'Siam')
+g.add_edge('China', 'Siberia')
+g.add_edge('China', 'Mongolia')
+g.add_edge('Siberia', 'Mongolia')
+g.add_edge('Siberia', 'Yakutsk')
+g.add_edge('Siberia', 'Irkutsk')
+g.add_edge('Mongolia', 'Irkutsk')
+g.add_edge('Mongolia', 'Kamchatka')
+g.add_edge('Mongolia', 'Japan')
+g.add_edge('Yakutsk', 'Irkutsk')
+g.add_edge('Yakutsk', 'Kamchatka')
+g.add_edge('Irkutsk', 'Kamchatka')
+g.add_edge('Kamchatka', 'Japan')
 
-class Graph(object):
-    """Graph class
+# Australia
+g.add_node('Indonesia', pos=[0.95, 0.7])
+g.add_node('New Guinea', pos=[1.0, 0.8])
+g.add_node('Western Australia', pos=[0.9, 0.9])
+g.add_node('Eastern Australia', pos=[1.0, 1.0])
+g.add_edge('Indonesia', 'New Guinea')
+g.add_edge('Indonesia', 'Western Australia')
+g.add_edge('New Guinea', 'Western Australia')
+g.add_edge('New Guinea', 'Eastern Australia')
+g.add_edge('Western Australia', 'Eastern Australia')
 
-       Attributes:
-           nodes (list(Node)): List of nodes
-           edges (list(Edge)): List of edges
-           c (drawille.Canvas): Canvas object
-    """
+# Connections
+g.add_edge('Siam', 'Indonesia', label='+')
+g.add_edge('Southern Europe', 'Middle East', label='+')
+g.add_edge('Ukraine', 'Ural', label='+')
+g.add_edge('Ukraine', 'Afghanistan', label='+')
+g.add_edge('Ukraine', 'Middle East', label='+')
+g.add_edge('North Africa', 'Western Europe', label='+')
+g.add_edge('North Africa', 'Southern Europe', label='+')
+g.add_edge('Egypt', 'Middle East', label='+')
+g.add_edge('Egypt', 'Southern Europe', label='+')
+g.add_edge('East Africa', 'Middle East', label='+')
+g.add_edge('Brazil', 'North Africa', label='+')
+g.add_edge('Alaska', 'Kamchatka', label='+')
+g.add_edge('Greenland', 'Iceland', label='+')
+g.add_edge('Central America', 'Venezuela', label='+')
 
-    def __init__(self):
-        self.nodes = {}
-        self.edges = []
-        self.c = Canvas()
-
-        self.max_x = 300
-        self.max_y = 125
-
-    def add_node(self, label, x=0, y=0):
-        self.nodes[label] = Node(x, y, label)
-
-    def add_edge(self, label0, label1, label=""):
-        if not (label0 in self.nodes): self.add_node(label0)
-        if not (label1 in self.nodes): self.add_node(label1)
-        self.edges.append(Edge(self.nodes[label0], self.nodes[label1], label))
-
-    def draw_node(self, n, width, height):
-        half_width = width // 2
-        half_height = height // 2
-        for i in range(width):
-            self.c.set((n.x - half_width) + i, n.y - half_height)
-            self.c.set((n.x - half_width) + i, n.y + half_height)
-        for i in range(height):
-            self.c.set(n.x - half_width, (n.y - half_height) + i)
-            self.c.set(n.x + half_width, (n.y - half_height) + i)
-        label = n.label
-        for key in n.att:
-            label += ', '+str(n.att[key])
-        self.c.set_text(n.x - half_width + 3, n.y, label)
-
-    def draw_edge(self, e, node_width, node_height):
-        x_diff = e.n1.x - e.n0.x
-        y_diff = e.n1.y - e.n0.y
-        l = sqrt(x_diff**2 + y_diff**2)
-        m = (y_diff / x_diff) if x_diff else 0
-        dx = x_diff / l if l else 0
-        dy = y_diff / l if l else 0
-        half_width = node_width // 2
-        half_height = node_height // 2
-        for i in range(int(l)):
-            x = e.n0.x + i*dx
-            y = e.n0.y + i*dy
-            if (
-                abs(x - e.n0.x) > half_width and
-                abs(x - e.n1.x) > half_width
-            ) or (
-                abs(y - e.n0.y) > half_height and
-                abs(y - e.n1.y) > half_height
-            ): self.c.set(x, y)
-        self.c.set_text(e.n0.x + (l // 2)*dx, e.n0.y + (l // 2)*dy, e.label)
-
-    def draw(self, node_width = 30, node_height = 10):
-        for label in self.nodes:
-            node = self.nodes[label]
-            node.x = float(node.x * self.max_x)
-            node.y = float(node.y * self.max_y)
-            self.draw_node(node, node_width, node_height)
-        for edge in self.edges:
-            self.draw_edge(edge, node_width, node_height)
-        print(self.c.frame())
-
-def get_risk_graph():
-    g = Graph()
-
-    # North America
-    g.add_node('Alaska', 0, 0)
-    g.add_node('Northwest Territory', 0.1, 0.1)
-    g.add_node('Greenland', 0.3, 0.05)
-    g.add_node('Alberta', 0.05, 0.2)
-    g.add_node('Ontario', 0.15, 0.3)
-    g.add_node('Quebec', 0.25, 0.25)
-    g.add_node('Western US', 0.05, 0.4)
-    g.add_node('Eastern US', 0.25, 0.45)
-    g.add_node('Central America', 0.15, 0.6)
-    g.add_edge('Alaska', 'Northwest Territory')
-    g.add_edge('Alaska', 'Alberta')
-    g.add_edge('Northwest Territory', 'Alberta')
-    g.add_edge('Northwest Territory', 'Ontario')
-    g.add_edge('Northwest Territory', 'Greenland')
-    g.add_edge('Alberta', 'Ontario')
-    g.add_edge('Alberta', 'Western US')
-    g.add_edge('Ontario', 'Greenland')
-    g.add_edge('Ontario', 'Quebec')
-    g.add_edge('Ontario', 'Western US')
-    g.add_edge('Ontario', 'Eastern US')
-    g.add_edge('Greenland', 'Quebec')
-    g.add_edge('Western US', 'Eastern US')
-    g.add_edge('Western US', 'Central America')
-    g.add_edge('Quebec', 'Eastern US')
-
-    # South America
-    g.add_node('Venezuela', 0.2, 0.7)
-    g.add_node('Brazil', 0.3, 0.85)
-    g.add_node('Peru', 0.15, 0.8)
-    g.add_node('Argentina', 0.2, 1.0)
-    g.add_edge('Venezuela', 'Brazil')
-    g.add_edge('Venezuela', 'Peru')
-    g.add_edge('Brazil', 'Peru')
-    g.add_edge('Brazil', 'Argentina')
-    g.add_edge('Peru', 'Argentina')
-
-    # Africa
-    g.add_node('North Africa', 0.45, 0.65)
-    g.add_node('Egypt', 0.55, 0.6)
-    g.add_node('East Africa', 0.65, 0.7)
-    g.add_node('Congo', 0.5, 0.8)
-    g.add_node('South Africa', 0.55, 1.0)
-    g.add_node('Madagascar', 0.6, 0.9)
-    g.add_edge('North Africa', 'Egypt')
-    g.add_edge('North Africa', 'East Africa')
-    g.add_edge('North Africa', 'Congo')
-    g.add_edge('Egypt', 'East Africa')
-    g.add_edge('East Africa', 'Congo')
-    g.add_edge('East Africa', 'Madagascar')
-    g.add_edge('Congo', 'South Africa')
-    g.add_edge('Madagascar', 'South Africa')
-
-    # Europe
-    g.add_node('Western Europe', 0.45, 0.5)
-    g.add_node('Southern Europe', 0.6, 0.45)
-    g.add_node('Northern Europe', 0.55, 0.35)
-    g.add_node('Great Britain', 0.4, 0.3)
-    g.add_node('Iceland', 0.4, 0.2)
-    g.add_node('Scandinavia', 0.55, 0.1)
-    g.add_node('Ukraine', 0.6, 0.2)
-    g.add_edge('Western Europe', 'Southern Europe')
-    g.add_edge('Western Europe', 'Northern Europe')
-    g.add_edge('Western Europe', 'Great Britain')
-    g.add_edge('Southern Europe', 'Northern Europe')
-    g.add_edge('Southern Europe', 'Ukraine')
-    g.add_edge('Northern Europe', 'Great Britain')
-    g.add_edge('Northern Europe', 'Ukraine')
-    g.add_edge('Northern Europe', 'Scandinavia')
-    g.add_edge('Great Britain', 'Scandinavia')
-    g.add_edge('Great Britain', 'Iceland')
-    g.add_edge('Ukraine', 'Scandinavia')
-    g.add_edge('Scandinavia', 'Iceland')
-
-    # Asia
-    g.add_node('Middle East', 0.7, 0.55)
-    g.add_node('Afghanistan', 0.75, 0.4)
-    g.add_node('India', 0.8, 0.6)
-    g.add_node('Ural', 0.75, 0.15)
-    g.add_node('China', 0.9, 0.5)
-    g.add_node('Siberia', 0.8, 0.2)
-    g.add_node('Mongolia', 0.95, 0.4)
-    g.add_node('Yakutsk', 0.85, 0.1)
-    g.add_node('Irkutsk', 0.9, 0.25)
-    g.add_node('Kamchatka', 1.0, 0.0)
-    g.add_node('Japan', 1.0, 0.2)
-    g.add_node('Siam', 0.9, 0.65)
-    g.add_edge('Middle East', 'Afghanistan')
-    g.add_edge('Middle East', 'India')
-    g.add_edge('Afghanistan', 'India')
-    g.add_edge('Afghanistan', 'Ural')
-    g.add_edge('Afghanistan', 'China')
-    g.add_edge('India', 'China')
-    g.add_edge('India', 'Siam')
-    g.add_edge('Ural', 'China')
-    g.add_edge('Ural', 'Siberia')
-    g.add_edge('China', 'Siam')
-    g.add_edge('China', 'Siberia')
-    g.add_edge('China', 'Mongolia')
-    g.add_edge('Siberia', 'Mongolia')
-    g.add_edge('Siberia', 'Yakutsk')
-    g.add_edge('Siberia', 'Irkutsk')
-    g.add_edge('Mongolia', 'Irkutsk')
-    g.add_edge('Mongolia', 'Kamchatka')
-    g.add_edge('Mongolia', 'Japan')
-    g.add_edge('Yakutsk', 'Irkutsk')
-    g.add_edge('Yakutsk', 'Kamchatka')
-    g.add_edge('Irkutsk', 'Kamchatka')
-    g.add_edge('Kamchatka', 'Japan')
-
-    # Australia
-    g.add_node('Indonesia', 0.95, 0.7)
-    g.add_node('New Guinea', 1.0, 0.8)
-    g.add_node('Western Australia', 0.9, 0.9)
-    g.add_node('Eastern Australia', 1.0, 1.0)
-    g.add_edge('Indonesia', 'New Guinea')
-    g.add_edge('Indonesia', 'Western Australia')
-    g.add_edge('New Guinea', 'Western Australia')
-    g.add_edge('New Guinea', 'Eastern Australia')
-    g.add_edge('Western Australia', 'Eastern Australia')
-
-    # Connections
-    g.add_edge('Siam', 'Indonesia')
-    g.add_edge('Southern Europe', 'Middle East')
-    g.add_edge('Ukraine', 'Ural')
-    g.add_edge('Ukraine', 'Afghanistan')
-    g.add_edge('Ukraine', 'Middle East')
-    g.add_edge('North Africa', 'Western Europe')
-    g.add_edge('North Africa', 'Southern Europe')
-    g.add_edge('Egypt', 'Middle East')
-    g.add_edge('Egypt', 'Southern Europe')
-    g.add_edge('East Africa', 'Middle East')
-    g.add_edge('Brazil', 'North Africa')
-    g.add_edge('Eastern US', 'Central America')
-    g.add_edge('Alaska', 'Kamchatka')
-    g.add_edge('Greenland', 'Iceland')
-    g.add_edge('Central America', 'Venezuela')
-
-    return g
-
-g = get_risk_graph()
-
-# Add armies
-for label in g.nodes:
-    node = g.nodes[label]
-    node.att['owner'] = 'Ethan'
-    node.att['armies'] = 13
-
-g.draw(0, 0)
+g.draw()
